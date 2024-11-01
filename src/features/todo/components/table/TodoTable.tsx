4ec19df1eb1c87b62dd3c1b5f7c9f153c053220f
denys_matsevych <matsevychd@gmail.com>
2024-11-01 10:24:08 +0200

    implemented edit, delete todo

--- src/features/todo/components/table/TodoTable.tsx
+++ src/features/todo/components/table/TodoTable.tsx
@@ -1,11 +1,24 @@
+import { ChangeEvent } from "react";
 import { Todo } from "../../service/todo.service";
 
 interface TodoTableProps {
   todoList: Todo[];
+  editTodo: Todo | null;
+  onTodoItemDelete: (id: number) => void;
+  onEditButtonClick: (row: Todo) => void;
+  onCancelEditTodo: () => void;
+  onTodoTitleChange: (event: ChangeEvent<HTMLInputElement>, id: number) => void;
+  onSaveTodoButtonClick: () => void;
 }
 
 const TodoTable = ({
-  todoList
+  todoList,
+  editTodo,
+  onTodoItemDelete,
+  onEditButtonClick,
+  onTodoTitleChange,
+  onSaveTodoButtonClick,
+  onCancelEditTodo,
 }: TodoTableProps) => {
   return (
     <table>
@@ -15,15 +28,49 @@ const TodoTable = ({
           <th>Todo</th>
           <th>Completed</th>
           <th>User Id</th>
+          <th>Actions</th>
         </tr>
       </thead>
       <tbody>
         {todoList.map((todo) => (
           <tr key={todo.id}>
             <td>{todo.id}</td>
-            <td>{todo.todo}</td>
+            <td>
+              {editTodo?.id === todo.id ? (
+                <input
+                  type="text"
+                  value={editTodo.todo}
+                  onChange={(event) => onTodoTitleChange(event, todo.id)}
+                />
+              ) : (
+                todo.todo
+              )}
+            </td>
             <td>{todo.completed ? "Yes" : "No"}</td>
             <td>{todo.userId}</td>
+            <td>
+              <div
+                style={{
+                  display: "flex",
+                  gap: "1em",
+                }}
+              >
+                {editTodo?.id === todo.id ? (
+                  <button onClick={onSaveTodoButtonClick}>Save</button>
+                ) : (
+                  <button onClick={() => onEditButtonClick(todo)}>Edit</button>
+                )}
+                {editTodo?.id === todo.id ? (
+                  <button onClick={() => onCancelEditTodo()}>
+                    Cancel
+                  </button>
+                ) : (
+                  <button onClick={() => onTodoItemDelete(todo.id)}>
+                    Delete
+                  </button>
+                )}
+              </div>
+            </td>
           </tr>
         ))}
       </tbody>
--- src/features/todo/components/table/TodoTableContainer.tsx
+++ src/features/todo/components/table/TodoTableContainer.tsx
@@ -1,14 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { AxiosError } from 'axios';
+import { AxiosError } from "axios";
 
-import { Todo, TodoService } from '../../service/todo.service';
-import TodoTable from './TodoTable';
+import { Todo, TodoService } from "../../service/todo.service";
+import TodoTable from "./TodoTable";
 
 const TodoTableContainer = () => {
   const [todoList, setTodoList] = useState<Todo[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+
+  const [editTodo, setEditTodo] = useState<Todo | null>(null);
 
   useEffect(() => {
     let isMounted = true;
@@ -33,23 +35,98 @@ const TodoTableContainer = () => {
       } finally {
         setLoading(false);
       }
-    }
+    };
 
     fetchTodos();
 
     return () => {
       isMounted = false;
       abortController.abort();
-    }
+    };
   }, []);
+
+  const handleTodoItemDelete = async (id: number) => {
+    try {
+      setLoading(true);
+
+      await new TodoService().deleteTodoById(id);
+
+      setTodoList((prev) => prev.filter((todo) => todo.id !== id));
+
+      setLoading(false);
+    } catch (error) {
+      setError((error as AxiosError).message);
+      setLoading(false);
+    }
+  };
+
+  const handleSaveTodoButtonClick = async () => {
+    try {
+      setLoading(true);
+
+      if (!editTodo) {
+        return;
+      }
+
+      await new TodoService().updateTodo(editTodo);
+
+      setTodoList((prev) =>
+        prev.map((todo) => {
+          if (todo.id === editTodo.id) {
+            return {
+              ...todo,
+              todo: editTodo.todo,
+            };
+          }
+
+          return todo;
+        })
+      );
+    } catch (error) {
+      setError((error as AxiosError).message);
+    }
+  };
+
+  const handleEditButtonClick = (row: Todo) => {
+    setEditTodo(row);
+  };
+
+  const handleTodoTitleChange = (event: React.ChangeEvent<HTMLInputElement>, id: number) => {
+    setEditTodo((prev) => {
+      if (!prev) {
+        return prev;
+      }
+
+      if (prev.id !== id) {
+        return prev;
+      }
+
+      return {
+        ...prev,
+        todo: event.target.value,
+      };
+    });
+  };
+
+  const handleCancelEditTodo = () => {
+    setEditTodo(null);
+  };
 
   return (
     <div>
-       {loading && <p>Loading...</p>}
-       {error && <p>Error: {error}</p>}
-       <TodoTable todoList={todoList} />
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      <TodoTable
+        todoList={todoList}
+        editTodo={editTodo}
+        onTodoItemDelete={handleTodoItemDelete}
+        onEditButtonClick={handleEditButtonClick}
+        onSaveTodoButtonClick={handleSaveTodoButtonClick}
+        onTodoTitleChange={handleTodoTitleChange}
+        onCancelEditTodo={handleCancelEditTodo}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default TodoTableContainer
+export default TodoTableContainer;
