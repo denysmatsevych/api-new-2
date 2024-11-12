b056846bcd7cdd23affc08f52cbd62bc6de6b266
denys_matsevych <matsevychd@gmail.com>
2024-11-12 13:21:13 +0200

    added Unuathorized page

---
+++ src/components/UnauthorizedPage.tsx
@@ -0,0 +1,11 @@
+
+const UnauthorizedPage = () => {
+  return (
+    <div>
+      <h1>Unauthorized</h1>
+      <p>You are not authorized to view this page.</p>
+    </div>
+  )
+}
+
+export default UnauthorizedPage
